dfb366ed0369a15a019b908f54a584c5aa59419f
jwddog <jwdafoe1@gmail.com>
2022-08-01 22:02:34 -0400

    Added documentation comments to NTFS file_record.py

--- src/NTFS/MFT/file_record.py
+++ src/NTFS/MFT/file_record.py
@@ -6,9 +6,44 @@ from src import common
 
 class FileRecord:
 
+    """
+    Reads and stores necessary metadata in the file record.
+
+    Attributes
+    ----------
+    header : RecordHeader
+        The header associated with the file record.
+        Contains useful flags about the file.
+    data : MFT.record_attributes.Data
+        The object associated with the $DATA attribute for the file.
+        Contains all of the data pointers for the file.
+    fileName : MFT.record_attributes.FileName
+        The object associated with the $FILE_NAME attribute for the file.
+        Contains the file name of the file.
+
+    Methods
+    -------
+    readAttributes(self, data: bytes, readPointers: bool)
+        reads the $DATA and $FILE_NAME attributes in the MFT file record.
+    """
+
     def __init__(self, data: bytes, bootSector: structures.boot_sector.BootSector, readPointers: bool, readAllAttr: bool):
 
-        self.header = RecordHeader(data)
+        """
+        Parameters
+        ----------
+        data : bytes
+            The data bytes for the MFT file record.
+        bootSector : structures.boot_sector.BootSector
+            The bootSector associated with the disk being used.
+        readPointers : bool
+            Indicates whether the data pointers should be read.
+        readAllAttr : bool
+            Indicates whether to read all of the attributes or not.
+            If thiis is false, or the file is not deleted, then only the header is read.
+        """
+
+        self.header: RecordHeader = RecordHeader(data)
         self.data: MFT.record_attributes.Data = False
         self.fileName: MFT.record_attributes.FileName = False
 
@@ -41,17 +76,33 @@ class FileRecord:
 
 class RecordHeader:
 
+    """
+    Reads data associated with a file record header.
+
+    Attributes
+    ----------
+    length : int
+        The length in bytes of the header.
+    isDeleted : bool
+        A flag indicating whether the record entry is associated with a deleted file, or not.
+    """
+
     def __init__(self, data: bytes):
+
+        """
+        Parameters
+        ----------
+        data : bytes
+        The data bytes associated with the record header.
+        """
+
         decoder = common.decode.Decoder()
 
-        self.length = decoder.leBytesToDecimal(data, 20, 20)
-        self.isDeleted = False
+        self.length: int = decoder.leBytesToDecimal(data, 20, 20)
+        self.isDeleted: bool = False
         flags = []
-        
+
         flags.extend(data[22:24])
         flags = flags[0]
         if (data[22]) == 0:
             self.isDeleted = True
-
-
-
